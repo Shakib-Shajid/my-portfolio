4bc86af51d0ad7c0376be6636dc0e43f27ec85d9
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-12-12 19:48:42 +0600

    projects added

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,71 +1,78 @@
+import { BsLinkedin } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa6";
+
 
 const Contact = () => {
     return (
-        <div className="flex justify-center items-center gap-10 ">
-            <div className="w-1/3">
-                <h3 className="text-3xl font-semibold text-white my-3">Contact with Me</h3>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam ducimus voluptatem nesciunt placeat, in soluta eum consectetur quibusdam, deserunt pariatur eveniet dolorum voluptatibus alias architecto est repellendus rem neque laudantium. Dolorem tempora qui cumque corporis harum placeat eius, dolores ex earum, deserunt est non voluptatem omnis dicta eaque reiciendis sapiente.</p>
-                <div className="text-white">
-                    <p className="border-2 p-4 my-3">Email: shakibalshajid1682@gmail.com</p>
-                    <p className="border-2 p-4 my-3">Email: shakibalshajid1682@gmail.com</p>
-                    <p className="border-2 p-4 my-3">Email: shakibalshajid1682@gmail.com</p>
-                </div>
-            </div>
-            <div className=" px-6 py-24 sm:py-32 lg:px-8 w-2/3">
-                <div className="mx-auto max-w-2xl text-center">
-                    <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Contact</h2>
-                </div>
-                <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
-                    <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-
-                        <div className="sm:col-span-2">
-                            <label htmlFor="company" className="block text-sm font-semibold leading-6 text-white">
-                                Full Name
-                            </label>
-                            <div className="mt-2.5">
-                                <input
-                                    type="text"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-                        <div>
-                            <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-white">
-                                Email
-                            </label>
-                            <div className="mt-2.5">
-                                <input
-                                    type="email"
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-                        <div>
-                            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-white">
-                                Phone Number
-                            </label>
-                            <div className="mt-2.5">
-                                <input
-                                    type="text" className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
-
-
-                        <div className="sm:col-span-2">
-                            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-white">
-                                Message
-                            </label>
-                            <div className="mt-2.5">
-                                <textarea
-                                    rows={4}
-                                    className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
+        <div className="my-12">
+            <h3 className="text-4xl font-semibold text-white text-center  mt-5">Contact with Me</h3>
+            <div className="flex justify-center items-center gap-10 ">
+                <div className="w-1/3 mt-10">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam ducimus voluptatem nesciunt placeat, in soluta eum consectetur quibusdam, deserunt pariatur eveniet dolorum voluptatibus alias architecto est repellendus rem neque laudantium. Dolorem tempora qui cumque corporis harum placeat eius, dolores ex earum, deserunt est non voluptatem omnis dicta eaque reiciendis sapiente.</p>
+                    <div className="text-white">
+                        <p className="rounded-lg border-2 p-4 my-3 flex items-center gap-3"><MdOutlineEmail />
+                            shakibalshajid1682@gmail.com</p>
+                        <p className="rounded-lg border-2 p-4 my-3 flex items-center gap-3"><BsLinkedin />
+                            Shakib Al Shajid</p>
+                        <p className="rounded-lg border-2 p-4 my-3 flex items-center gap-3"><FaGithub />
+                            shakib-shajid</p>
                     </div>
-                    <button className="btn btn-info  w-full mt-5 text-white">Send Message</button>
-                </form>
+                </div>
+                <div className=" px-6 lg:px-8 w-2/3">
+
+                    <form className="mx-auto mt-10 max-w-xl">
+                        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+
+                            <div className="sm:col-span-2">
+                                <label htmlFor="company" className="block text-sm font-semibold leading-6 text-white">
+                                    Full Name
+                                </label>
+                                <div className="mt-2.5">
+                                    <input
+                                        type="text"
+                                        className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-white">
+                                    Email
+                                </label>
+                                <div className="mt-2.5">
+                                    <input
+                                        type="email"
+                                        className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-white">
+                                    Phone Number
+                                </label>
+                                <div className="mt-2.5">
+                                    <input
+                                        type="text" className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+
+
+                            <div className="sm:col-span-2">
+                                <label htmlFor="message" className="block text-sm font-semibold leading-6 text-white">
+                                    Message
+                                </label>
+                                <div className="mt-2.5">
+                                    <textarea
+                                        rows={4}
+                                        className="block w-full rounded-md border-0 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <button className="btn btn-info  w-full mt-5 text-white">Send Message</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
 
 const Projects = () => {
     return (
         <div>
-            <h3 className="text-3xl font-semibold text-center my-10">My Projects</h3>
+            <h3 className="text-3xl font-semibold text-center my-10 text-white ">My Projects</h3>
             <div className="grid grid-cols-2">
                 <div className="card card-compact w-[500px] bg-base-100 shadow-xl border-blue-600 border-2 ">
                     <figure><img src="../../public/project-1.png" alt="Shoes" /></figure>
@@ -10,10 +11,34 @@ const Projects = () => {
                         <h2 className="card-title text-white">Career Fair</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
                         <div className="card-actions mt-3">
-                            <button className="btn btn-info ">Live Link</button>
-                            <button className="btn btn-info ">Server Code</button>
-                            <button className="btn btn-info ">Client Codes</button>
-                            <button className="btn btn-info ">Details</button>
+                            <Link target="_blank" to="https://react-career-fair.web.app/"><button className="btn btn-info">Live Link</button></Link>
+                            <Link target="_blank" to="https://github.com/Shakib-Shajid/react-career-fair" ><button className="btn btn-info">Source Code</button></Link>
+                            {/* <button className="btn btn-info">Details</button> */}
+                            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                            <button className="btn btn-info" onClick={() => document.getElementById('my_modal_3').showModal()}>Details</button>
+                            <dialog id="my_modal_3" className="modal">
+                                <div className="modal-box">
+                                    <form method="dialog">
+                                        {/* if there is a button in form, it will close the modal */}
+                                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                    </form>
+                                    <h3 className="text-3xl font-semibold text-white my-3">Features of my website</h3>
+                                    <ul className="text-white ml-4 list-decimal" >
+                                        <li>You can see all the services we provide.</li>
+                                        <li>You can ask any question.</li>
+                                        <li>Show all the details of events.</li>
+                                        <li>Login and registration system.</li>
+                                        <li>Can not see sensitive info without login.</li>
+                                    </ul>
+                                    <h3 className="text-3xl font-semibold text-white my-3">Technology</h3>
+                                    <ul className="text-white ml-4 list-decimal" >
+                                        <li>React</li>
+                                        <li>Firebase</li>
+                                        <li>Tailwind css</li>
+                                        <li>Daisy UI</li>
+                                    </ul>
+                                </div>
+                            </dialog>
                         </div>
                     </div>
                 </div>
@@ -23,10 +48,32 @@ const Projects = () => {
                         <h2 className="card-title text-white">Weather API</h2>
                         <p>If a dog chews shoes whose shoes does he choose?</p>
                         <div className="card-actions mt-3">
-                            <button className="btn btn-info ">Live Link</button>
-                            <button className="btn btn-info ">Server Code</button>
-                            <button className="btn btn-info ">Client Codes</button>
-                            <button className="btn btn-info ">Details</button>
+                            <Link target="_blank" to="https://weather-api-taupe.vercel.app/"><button className="btn btn-info">Live Link</button></Link>
+                            <Link target="_blank" to="https://github.com/Shakib-Shajid/weather-api" ><button className="btn btn-info">Source Code</button></Link>
+                            {/* You can open the modal using document.getElementById('ID').showModal() method */}
+                            <button className="btn btn-info" onClick={() => document.getElementById('my_modal_2').showModal()}>Details</button>
+                            <dialog id="my_modal_2" className="modal">
+                                <div className="modal-box">
+                                    <form method="dialog">
+                                        {/* if there is a button in form, it will close the modal */}
+                                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                                    </form>
+                                    <h3 className="text-3xl font-semibold text-white my-3">Features of my website</h3>
+                                    <ul className="text-white ml-4 list-decimal" >
+                                        <li>You can see over the world temperature.</li>
+                                        <li>You can see those by icons</li>
+                                        <li>Show here date and time also</li>
+                                        <li>You can see another info like sunset, pressure etc.</li>
+                                        <li>You can see country name.</li>
+                                    </ul>
+                                    <h3 className="text-3xl font-semibold text-white my-3">Technology</h3>
+                                    <ul className="text-white ml-4 list-decimal" >
+                                        <li>React</li>
+                                        <li>CSS 3</li>
+                                        <li>HTML 5</li>
+                                    </ul>
+                                </div>
+                            </dialog>
                         </div>
                     </div>
                 </div>
