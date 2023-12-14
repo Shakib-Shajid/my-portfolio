8515c7f78863804235a6646a23619c92cee54fb5
Shakib-Shajid <shakibalshajid1682@gmail.com>
2023-12-15 00:39:16 +0600

    cursor effect added

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -3,6 +3,8 @@ import Contact from "./Contact";
 import Home from "./Home";
 import Projects from "./Projects";
 import Skills from "./Skills";
+import AnimatedCursor from "react-animated-cursor"
+
 
 const Main = () => {
     return (
@@ -12,6 +14,33 @@ const Main = () => {
             <Skills></Skills>
             <Projects></Projects>
             <Contact></Contact>
+            {/* <AnimatedCursor
+                innerSize={8}
+                outerSize={35}
+                innerScale={1}
+                outerScale={2}
+                outerAlpha={0}
+                hasBlendMode={true}
+                innerStyle={{
+                    backgroundColor: 'var(--cursor-color)'
+                }}
+                outerStyle={{
+                    border: '3px solid var(--cursor-color)'
+                }}
+            /> */}
+            <AnimatedCursor
+                innerSize={8}
+                outerSize={35}
+                innerScale={1}
+                outerScale={1.7}
+                outerAlpha={0}
+                outerStyle={{
+                    border: '3px solid var(--cursor-color)'
+                }}
+                innerStyle={{
+                    backgroundColor: 'var(--cursor-color)'
+                }}
+            />
         </div>
     );
 };
